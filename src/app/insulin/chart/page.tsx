119a3c8af46f8f82dd039633c76392f58ee14d68
Max Kalhama <kalhama.max@gmail.com>
2024-08-11 14:33:10 +0300

    fix: comment out IOB chart as it's not working as wer're migrating away from sql view and rely on straight on queries

--- src/app/insulin/chart/page.tsx
+++ src/app/insulin/chart/page.tsx
@@ -1,10 +1,11 @@
 import { db } from '@/db'
-import { insulin_on_board } from '@/schema'
 
+// import { insulin_on_board } from '@/schema'
 import { InsulinOnBoard } from './InsulinOnBoard'
 
 export default async function InsulinOnBoardProvider() {
-  const data = await db.select().from(insulin_on_board)
+  return <div>WIP</div>
+  // const data = await db.select().from(insulin_on_board)
 
-  return <InsulinOnBoard data={data} />
+  // return <InsulinOnBoard data={data} />
 }
