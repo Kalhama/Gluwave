8e939c56a69ba5fb8e97fff9b92637fedd6a2030
Max Kalhama <kalhama.max@gmail.com>
2024-08-29 22:57:16 +0300

    commas and semicolons

--- nextjs/query_all_metrics_at_any_timeframe.sql
+++ nextjs/query_all_metrics_at_any_timeframe.sql
@@ -54,7 +54,7 @@ BEGIN
 
     -- If interpolated range is too long just use last known value
     IF (minutes_between(x2, x1) > 15) THEN
-      RETURN y1
+      RETURN y1;
     END IF;
 
     return y1 + (y2 - y1) / EXTRACT(EPOCH FROM(x2 - x1)) * EXTRACT (EPOCH FROM (t - x1));
@@ -64,8 +64,8 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION observed_carbs(
   glucose_chnage DOUBLE PRECISION, 
   insulin_change DOUBLE PRECISION, 
-  ISF => DOUBLE PRECISION,
-  ICR => DOUBLE PRECISION
+  ISF DOUBLE PRECISION,
+  ICR DOUBLE PRECISION
 )
 RETURNS DOUBLE PRECISION AS $$
 BEGIN
@@ -85,13 +85,13 @@ WITH timeframe AS (
   -- WHERE "userId" = '123'
   -- AND timestamp > '1970-01-01'::timestamp
   -- AND timestamp < '2020-01-01'::timestamp
-  UNION SELECT timestamp AS timestamp
-  FROM glucose
+  -- UNION SELECT timestamp AS timestamp
+  -- FROM glucose
   -- WHERE "userId" = '123'
   -- AND timestamp > '1970-01-01'::timestamp
   -- AND timestamp < '2020-01-01'::timestamp
-  ORDER BY timestamp DESC
-  LIMIT 1
+  -- ORDER BY timestamp DESC
+  -- LIMIT 1 -- TODO fix this limits all results to 1
   -- UNION SELECT '1970-01-01'::timestamp as timestamp -- add "from" to series
 ), interpolated_glucose AS (
   SELECT 
@@ -102,7 +102,7 @@ WITH timeframe AS (
       x2 => glucose.next_timestamp, 
       y1 => glucose.amount,
       y2 => glucose.next_amount
-    )) AS glucose,
+    )) AS glucose
   FROM timeframe
   LEFT JOIN (
     SELECT 
@@ -124,7 +124,7 @@ WITH timeframe AS (
       start => carbs.timestamp, 
       amount => carbs.amount, 
       decay => carbs.decay
-    )), 0) AS total_carbs_absorbed,
+    )), 0) AS total_carbs_absorbed
   FROM timeframe
   LEFT JOIN carbs 
     ON carbs.timestamp < timeframe.timestamp 
@@ -145,29 +145,30 @@ WITH timeframe AS (
     ON insulin.timestamp < timeframe.timestamp
     -- AND insulin.userId = '123'
   GROUP BY timeframe.timestamp
-), combined_metrics AS (
+), basic_metrics AS (
   SELECT 
     timeframe.timestamp,
-    ANY_VALUE(total_insulin_absorbed) - FIRST_VALUE(total_insulin_absorbed) OVER (ORDER BY timeframe.timestamp) AS total_insulin_absorbed,
-    ANY_VALUE(total_carbs_absorbed) - FIRST_VALUE(total_carbs_absorbed) OVER (ORDER BY timeframe.timestamp) AS total_carbs_absorbed_predicted,
-    ANY_VALUE(glucose) AS glucose,
+    ANY_VALUE(total_insulin_absorbed) - FIRST_VALUE(ANY_VALUE(total_insulin_absorbed)) OVER (ORDER BY timeframe.timestamp) AS insulin_absorbed,
+    ANY_VALUE(total_carbs_absorbed) - FIRST_VALUE(ANY_VALUE(total_carbs_absorbed)) OVER (ORDER BY timeframe.timestamp) AS carbs_absorbed_predicted,
+    ANY_VALUE(glucose) AS glucose
   FROM timeframe
-  LEFT JOIN ON timeframe.timestamp = aggregated_insulin.timestamp
-  LEFT JOIN ON timeframe.timestamp = aggregated_carbs.timestamp
-  LEFT JOIN ON timeframe.timestamp = interpolated_glucose.timestamp
-  GROUP BY timestamp
-  ORDER BY timestamp ASC
-), with_observed_carbs AS (
+  LEFT JOIN aggregated_insulin ON timeframe.timestamp = aggregated_insulin.timestamp
+  LEFT JOIN aggregated_carbs ON timeframe.timestamp = aggregated_carbs.timestamp
+  LEFT JOIN interpolated_glucose ON timeframe.timestamp = interpolated_glucose.timestamp
+  GROUP BY timeframe.timestamp
+  ORDER BY timeframe.timestamp ASC
+), metrics AS (
   SELECT
     *,
     observed_carbs(
-      glucose_chnage => LEAD(glucose) - glucose 
-      insulin_change => total_insulin_absorbed - LEAD(total_insulin_absorbed),
+      glucose_chnage => LEAD(glucose) OVER (ORDER BY timestamp) - glucose,
+      insulin_change => insulin_absorbed - LEAD(insulin_absorbed) OVER (ORDER BY timestamp),
       ISF => 2,
       ICR => 10
-    ) AS observed_carbs,
-  FROM combined_aggregated
-), carbs_with_attributed_observed_carbs AS (
+    ) AS carbs_absorbed_observed,
+	LEAD(timestamp) OVER (ORDER BY timestamp) - timestamp AS interval
+  FROM basic_metrics
+), observed_carbs_per_meal AS (
   /* 
   TODO we can attribute carbs to only meals that are decaying. 
   
@@ -181,20 +182,20 @@ WITH timeframe AS (
   There might me need for two decays 1) for making predictions 2) for attributing observed carbs to meals. However it might be possible to combine these two.
   */
   SELECT
-    timeframe.timestamp as timestamp,
+    metrics.timestamp as timestamp,
     id,
     decay,
-    value,
-    value / decay as minimium_absorption_rate,
-    SUM(value / decay) OVER (PARTITION BY timestamp) AS total_minimium_absorption_rate /*if there are multiple meal entries for one timestamp, sum all of their minimum_absorption_rates */
-    observed_carbs * (minimium_absorption_rate / total_minimium_absorption_rate) AS observed_carbs_for_this_meal_at_this_time -- to hell with these names
-  FROM with_observed_carbs
+    amount,
+    amount / decay as minimium_absorption_rate,
+    SUM(amount / decay) OVER (PARTITION BY metrics.timestamp) AS total_minimium_absorption_rate, /*if there are multiple meal entries for one timestamp, sum all of their minimum_absorption_rates */
+    carbs_absorbed_observed * ((amount / decay) / SUM(amount / decay) OVER (PARTITION BY metrics.timestamp)) AS observed_carbs_for_this_meal_at_this_time -- to hell with these names
+  FROM metrics
   LEFT JOIN carbs
-    ON timeframe.timestamp >= carbs.timestamp 
-    AND timeframe.timestamp < carbs.timestamp + MAKE_INTERVAL(mins => carbs.decay)
+    ON metrics.timestamp >= carbs.timestamp 
+    AND metrics.timestamp < carbs.timestamp + MAKE_INTERVAL(mins => carbs.decay)
     -- AND carbs."userId" = '123'
 
-    -- Two possible use cases for this query
     -- GROUP BY observed_carbs_for_this_meal_at_this_time -- to get per meal end value 
-    -- WHERE id = '123 -- to get observed absorption over time for one meal
 )
+
+SELECT * FROM observed_carbs_per_meal
