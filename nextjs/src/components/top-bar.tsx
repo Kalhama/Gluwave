c3a99939478d6b28504d209b0a44c61f063e3f60
Max Kalhama <kalhama.max@gmail.com>
2024-09-14 18:18:13 +0300

    finish top bar

--- nextjs/src/components/top-bar.tsx
+++ nextjs/src/components/top-bar.tsx
@@ -1,11 +1,45 @@
 import { validateRequest } from '@/auth'
 import { db } from '@/db'
 import { glucose } from '@/schema'
-import { differenceInHours, differenceInMinutes, subMinutes } from 'date-fns'
-import { and, desc, eq, gte } from 'drizzle-orm'
-import { timestamp } from 'drizzle-orm/pg-core'
+import { differenceInMinutes, subMinutes } from 'date-fns'
+import { and, desc, eq, gte, lte, ne } from 'drizzle-orm'
 import { MoveRight } from 'lucide-react'
 import { redirect } from 'next/navigation'
+
+const glucoseTrend = async (last: {
+  value: number
+  id: number
+  timestamp: Date
+  userId: string
+  device: string | null
+}) => {
+  if (!last) return null
+
+  const [first] = await db
+    .select()
+    .from(glucose)
+    .where(
+      and(
+        eq(glucose.userId, last.userId),
+        gte(glucose.timestamp, subMinutes(last.timestamp, 45)),
+        lte(glucose.timestamp, subMinutes(last.timestamp, 10)),
+        ne(glucose.id, last.id)
+      )
+    )
+    .orderBy(desc(glucose.timestamp))
+    .limit(1)
+
+  if (!first) return null
+
+  const slope =
+    (last.value - first.value) /
+    differenceInMinutes(last.timestamp, first.timestamp)
+
+  // Multiply slope by magic number to make the results look good
+  const trend = ((Math.atan(slope * 12) * 180) / Math.PI) * -1
+
+  return trend
+}
 
 export const TopBar = async () => {
   const { user } = await validateRequest()
@@ -13,40 +47,46 @@ export const TopBar = async () => {
     redirect('/login')
   }
 
-  const tailGlucose = await db
-    .select({
-      timestamp: glucose.timestamp,
-      value: glucose.value,
-    })
+  const [last] = await db
+    .select()
     .from(glucose)
-    .where(
-      and(
-        eq(glucose.userId, user.id),
-        gte(glucose.timestamp, subMinutes(new Date(), 20))
-      )
-    )
+    .where(and(eq(glucose.userId, user.id)))
     .orderBy(desc(glucose.timestamp))
+    .limit(1)
 
-  const first = tailGlucose[tailGlucose.length - 1]
-  const last = tailGlucose[0]
+  if (!last) return null
 
-  if (!first || !last) return null
+  const trend = await glucoseTrend(last)
 
   const minutesDelta = differenceInMinutes(new Date(), last.timestamp)
-
-  const trend = (last.value - first.value) / (minutesDelta / 60)
-
-  const trendDegrees = (Math.atan(trend) * 180) / Math.PI
+  const stale = minutesDelta > 30
+  const status = (() => {
+    if (stale) return 'border-slate-300'
+    else if (last.value < 3.8) return 'border-red-600'
+    else if (last.value > 11) return 'border-orange-500'
+    else return 'border-green-600'
+  })()
 
   return (
-    <div className="border-b p-4 flex justify-between items-center">
-      <div className="rounded-full inline p-2">
-        <span className="text-2xl font-bold">{last.value}</span>
-        <span> mmol/l</span>
-        <MoveRight style={{ rotate: `${trendDegrees * -1}deg` }} />
+    <div className="border-b px-4 py-2 flex justify-between items-center">
+      <div
+        className={`flex items-center border-4 px-4 py-1 rounded-full ${status}`}
+      >
+        <div className="rounded-full inline">
+          <span className="text-2xl font-bold">{last.value}</span>
+          <span className="text-sm"> mmol/l</span>
+        </div>
+        {trend !== null && (
+          <MoveRight className="ml-2" style={{ rotate: `${trend}deg` }} />
+        )}
       </div>
       <div>
-        Last value {minutesDelta > 60 ? '>60' : minutesDelta} minutes ago
+        {stale && (
+          <>
+            Last value {minutesDelta > 60 ? 'over 60' : minutesDelta} minutes
+            ago
+          </>
+        )}
       </div>
     </div>
   )
