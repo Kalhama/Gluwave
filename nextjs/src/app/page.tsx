1ccc130c838d7c4aa77fec84c6001198e2f93b57
Max Kalhama <kalhama.max@gmail.com>
2024-09-20 02:08:45 +0300

    feat: <title>

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,8 +1,41 @@
+import { validateRequest } from '@/auth'
 import { AutoReload } from '@/components/auto-reload'
 import BloodGlucoseProvider from '@/components/blood-glucose'
 import { CarbsOnBoard } from '@/components/carbs-on-board'
 import InsulinOnBoardProvider from '@/components/insulin-on-board'
 import { TopBar } from '@/components/top-bar'
+import { db } from '@/db'
+import { glucose } from '@/schema'
+import { and, desc, eq } from 'drizzle-orm'
+import { Metadata } from 'next'
+import { redirect } from 'next/navigation'
+
+const getLastGlucose = async (userId: string) => {
+  const [last] = await db
+    .select()
+    .from(glucose)
+    .where(and(eq(glucose.userId, userId)))
+    .orderBy(desc(glucose.timestamp))
+    .limit(1)
+
+  return last
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const { user } = await validateRequest()
+  if (!user) {
+    redirect('/login')
+  }
+
+  const last = await getLastGlucose(user.id)
+
+  return {
+    title: `${last.value.toLocaleString(undefined, {
+      minimumFractionDigits: 1,
+      maximumFractionDigits: 1,
+    })} mmol/l`,
+  }
+}
 
 export default async function App() {
   return (
