55ddf98f76fa0e55662b0a878871fbd8f6707750
Max Kalhama <kalhama.max@gmail.com>
2024-09-30 21:35:50 +0300

    fix metrics schema

--- gluwave/src/schema.ts
+++ gluwave/src/schema.ts
@@ -76,54 +76,37 @@ export const metrics = pgView('metrics', {
   total_insulin_absorbed: doublePrecision('total_insulin_absorbed'),
   observed_carbs: doublePrecision('observed_carbs'),
 }).as(sql`
-WITH glucose_rate AS (
-  -- TODO limit amount of values we get if they are really dense. 5 mins should suffice.
-  SELECT
-    id as glucose_id,
-    user_id,
-    timestamp,
-    "amount" as glucose,
-    LEAD(amount) OVER (PARTITION BY "user_id" ORDER BY timestamp ) - "amount" AS glucose_change,
-    LEAD(timestamp) OVER (PARTITION BY "user_id" ORDER BY timestamp ) - "timestamp" AS step
-  FROM 
-    glucose
-), insulin_rate AS (
-  SELECT
-    glucose_id,
-    total_insulin_absorbed,
-    LEAD(total_insulin_absorbed) OVER (PARTITION BY "user_id" ORDER BY timestamp ) - "total_insulin_absorbed" AS insulin_absorbed
-  FROM (
-    SELECT
-      glucose.user_id,
-      glucose.timestamp,
-      glucose.id as glucose_id,
-      COALESCE(SUM(total_insulin_absorbed(
-        t => glucose.timestamp,
-        start => insulin.timestamp,
-        amount => insulin.amount
-      )), 0) as total_insulin_absorbed
-    FROM glucose
-    LEFT JOIN insulin 
-      ON glucose.user_id = insulin.user_id 
-      AND insulin.timestamp <= glucose.timestamp 
-    GROUP BY glucose.id
-  )
-)
-
-SELECT 
-  glucose_rate.glucose_id,
-  LEAD(glucose_rate.glucose_id) OVER (PARTITION BY user_id ORDER BY timestamp) AS next_glucose_id,
-  user_id,
-  timestamp,
-  glucose,
-  glucose_change,
-  step,
-  total_insulin_absorbed,
-  glucose_change / "user"."correctionRatio" * "user"."carbohydrateRatio" + insulin_absorbed * "user"."carbohydrateRatio" AS observed_carbs
-FROM glucose_rate 
-LEFT JOIN insulin_rate 
-ON insulin_rate.glucose_id = glucose_rate.glucose_id
-LEFT JOIN "user"
-ON "user"."id" = glucose_rate.user_id
-ORDER BY timestamp
+ WITH glucose_rate AS (
+         SELECT glucose.id AS glucose_id,
+            glucose.user_id,
+            glucose."timestamp",
+            glucose.amount AS glucose,
+            COALESCE(lead(glucose.amount) OVER (PARTITION BY glucose.user_id ORDER BY glucose."timestamp") - glucose.amount, 0::double precision) AS glucose_change,
+            lead(glucose."timestamp") OVER (PARTITION BY glucose.user_id ORDER BY glucose."timestamp") - glucose."timestamp" AS step
+           FROM glucose
+        ), insulin_rate AS (
+         SELECT unnamed_subquery.glucose_id,
+            unnamed_subquery.total_insulin_absorbed,
+            lead(unnamed_subquery.total_insulin_absorbed) OVER (PARTITION BY unnamed_subquery.user_id ORDER BY unnamed_subquery."timestamp") - unnamed_subquery.total_insulin_absorbed AS insulin_absorbed
+           FROM ( SELECT glucose.user_id,
+                    glucose."timestamp",
+                    glucose.id AS glucose_id,
+                    COALESCE(sum(total_insulin_absorbed(t => glucose."timestamp", start => insulin."timestamp", amount => insulin.amount)), 0::numeric) AS total_insulin_absorbed
+                   FROM glucose
+                     LEFT JOIN insulin ON glucose.user_id = insulin.user_id AND insulin."timestamp" <= glucose."timestamp"
+                  GROUP BY glucose.id) unnamed_subquery
+        )
+ SELECT glucose_rate.glucose_id,
+    lead(glucose_rate.glucose_id) OVER (PARTITION BY glucose_rate.user_id ORDER BY glucose_rate."timestamp") AS next_glucose_id,
+    glucose_rate.user_id,
+    glucose_rate."timestamp",
+    glucose_rate.glucose,
+    glucose_rate.glucose_change,
+    glucose_rate.step,
+    insulin_rate.total_insulin_absorbed,
+    COALESCE(glucose_rate.glucose_change / "user"."correctionRatio" * "user"."carbohydrateRatio" + insulin_rate.insulin_absorbed::double precision * "user"."carbohydrateRatio", 0::double precision) AS observed_carbs
+   FROM glucose_rate
+     LEFT JOIN insulin_rate ON insulin_rate.glucose_id = glucose_rate.glucose_id
+     LEFT JOIN "user" ON "user".id = glucose_rate.user_id
+  ORDER BY glucose_rate."timestamp";
 `)
